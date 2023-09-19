7856da418c056d9087fc26c0ca8197a3f6dfa9e4
minhTheGuy <134134741+minhTheGuy@users.noreply.github.com>
2023-09-19 11:27:18 +0700

    Delete TestMyQueue.java

--- TestMyQueue.java
+++
@@ -1,8 +0,0 @@
-public class TestMyQueue {
-    public static void main(String[] args) {
-        QueueInterface<Integer> queue = new MyQueue<Integer>();
-        queue.enQueue(3);
-        queue.enQueue(4);
-        System.out.println(queue.contains(45));
-    }
-}
