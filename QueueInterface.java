71436976acc4b84d5fae261656c79b5582f25da4
minhTheGuy <134134741+minhTheGuy@users.noreply.github.com>
2023-09-19 11:26:59 +0700

    Delete QueueInterface.java

--- QueueInterface.java
+++
@@ -1,9 +0,0 @@
-public interface QueueInterface<E> {
-    public void enQueue(E item);
-    public E deQueue();
-    public int size();
-    public boolean contains(E item);
-    public void print();
-    public boolean isEmpty();
-    public E getFront();
-}
